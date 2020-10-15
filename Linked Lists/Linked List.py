99eb22853d0df2ea324219b504d5cb2e158b0104
Sachin Singh <33282881+idowhatido@users.noreply.github.com>
2020-10-15 15:22:45 +0530

    Added Linked List

---
+++ Linked Lists/Linked List.py
@@ -0,0 +1,30 @@
+class Node:
+  def __init__(self,data):
+    self.data = data
+    self.next = None
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+
+  def printL(self):
+    itr = self.head
+    string = ''
+    while itr:
+      string += str(itr.data) + ' --> '
+      itr = itr.next
+    print(string)
+
+if __name__ == '__main__':
+    LinkedList1 = LinkedList()
+    Node1 = Node(15)
+    LinkedList1.head = Node1
+
+    Node2 = Node(12)
+    Node3 = Node(4)
+
+    Node1.next = Node2
+    Node2.next = Node3
+
+    LinkedList1.printL()
+
